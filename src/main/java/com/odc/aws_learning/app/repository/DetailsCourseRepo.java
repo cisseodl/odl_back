8b1b82c27fada3db54e05c3298420b822499e6dc
Artifique <fombadaouda72@gmail.com>
2026-02-10 06:46:52 +0000

    Fix: Retrait condition activate de la requÃªte apprenants par instructeur pour corriger affichage Mes Apprenants

--- src/main/java/com/odc/aws_learning/app/repository/DetailsCourseRepo.java
+++ src/main/java/com/odc/aws_learning/app/repository/DetailsCourseRepo.java
@@ -29,7 +29,7 @@ public interface DetailsCourseRepo extends JpaRepository<DetailsCourse, Long> {
     @Query("SELECT DISTINCT dc.learner.id FROM DetailsCourse dc " +
            "JOIN dc.course c " +
            "JOIN c.instructor i " +
-           "WHERE i.id = :instructorId AND dc.activate = true")
+           "WHERE i.id = :instructorId")
     List<Long> findDistinctLearnerIdsByInstructorId(@Param("instructorId") Long instructorId);
 
     @Query("SELECT new com.odc.aws_learning.app.dto.UserCourseCompletionStats(dc.learner.id, COUNT(dc.id)) FROM DetailsCourse dc WHERE dc.completed = true GROUP BY dc.learner.id")
